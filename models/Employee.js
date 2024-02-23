0532b32efa2f02d011399388e49f147da19ed41e
Zohaib <154052553+DotZohaib@users.noreply.github.com>
2024-02-23 09:00:19 +0500

    Add files via upload
    database Dummy

---
+++ models/Employee.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    name: String,
+    salary: Number,
+    language: String,
+    city: String,
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+module.exports = Employee
